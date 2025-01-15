e756ddc62d2b6ae405818cc0552f6e0c737c4f7f
sandermelles@gmail.com <sandermelles@gmail.com>
2025-01-15 03:28:42 +0200

    Models and SQLite

---
+++ Models/TrainingSession.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+
+namespace TrainingApp.Models
+{
+    public class TrainingSession
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Models/TrainingSessionExercise.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace TrainingApp.Models
+{
+    public class TrainingSessionExercise
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int TrainingSessionId { get; set; } // Foreign key
+        public string ExerciseName { get; set; }
+        public int Reps { get; set; }
+        public int Sets { get; set; }
+        public double Weight { get; set; }
+    }
+}
